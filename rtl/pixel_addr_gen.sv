3c3f324eb939586f56bbb8827861018bb9b51e54
camilleleute <camilleleute03@gmail.com>
2025-05-09 18:13:10 -0700

    pixel addr gen

--- rtl/pixel_addr_gen.sv
+++ rtl/pixel_addr_gen.sv
@@ -6,11 +6,37 @@ import displayConsts::*;
 import vgaTimes::*;
 
 module pixel_addr_gen (
+    input logic clk,
+    input logic rst,
+
     input logic [H_BITS-1:0] h_cnt,
     input logic [V_BITS-1:0] v_cnt,
     input logic in_frame, next, 
     output logic [PIXEL_ADDR_BITS-1:0] pixel_addr
     );
+
+    logic [PIXEL_ADDR_BITS-1:0] pixel_base;
+
+    // update pixel_base
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            pixel_base <= 0;
+        end else if (next && !in_frame) begin
+            if (v_cnt == V_VISIBLE_AREA - 1) begin
+                pixel_base <= 0;
+            end else begin
+                pixel_base <= pixel_base + H_VISIBLE_AREA;
+            end
+        end
+    end
+
+    // output pixel_addr
+    always_comb begin
+        if (in_frame) begin
+            pixel_addr = pixel_base + h_cnt;
+        end else begin
+            pixel_addr = 0;
+        end
+    end
     
-    assign pixel_addr = 0;
 endmodule
