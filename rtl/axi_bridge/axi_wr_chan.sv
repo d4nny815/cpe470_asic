7b8cf5dfdeea68a316f470906dea8fb2b637fc07
danny <d4nny815@gmail.com>
2025-05-12 03:53:39 -0700

    write channel passes verilator and icarus

--- rtl/axi_bridge/axi_wr_chan.sv
+++ rtl/axi_bridge/axi_wr_chan.sv
@@ -18,39 +18,33 @@ module axi_wr_chan (
     output logic wr_valid
     );
 
-
     // * =======================================================================
-    // * CONTROL PATH
+    // * Internal Signals
     // * =======================================================================
-
     typedef enum logic [1:0] {
-        READY,
-        VALID,
-        WAIT_RESP
+        READY       = 2'b00,
+        VALID       = 2'b01,
+        WAIT_RESP   = 2'b10
     } state_t;
     state_t PS, NS;
 
     always_ff @(posedge axi_clk or negedge reset_n) begin
         if (!reset_n) PS <= READY;
-        else PS <= NS;
+        else          PS <= NS;
     end
-    
-    logic awvalid_ready, wvalid_ready, b_ready;
-    
+
+    // * =======================================================================
+    // * CONTROL PATH
+    // * =======================================================================
+
     logic awready_r, wready_r, bvalid_r;
     BRESP_t bresp_r;
+    logic wr_addr_we, wr_data_we;
 
-    assign awvalid_ready = wr_chan_i.awvalid && awready_r;
-    assign wvalid_ready  = wr_chan_i.wvalid && wready_r;
-    assign b_ready = wr_chan_i.bready;
-
-    
     assign wr_chan_o.awready = awready_r;
     assign wr_chan_o.wready  = wready_r;
     assign wr_chan_o.bvalid  = bvalid_r;
     assign wr_chan_o.bresp   = bresp_r;
-
-    logic wr_addr_we, wr_data_we;
 
     always_comb begin
         awready_r = 0;
@@ -65,7 +59,7 @@ module axi_wr_chan (
             READY: begin
                 awready_r = 1;
                 wready_r = 1;
-                if (awvalid_ready && wvalid_ready) begin 
+                if (wr_chan_i.awvalid && wr_chan_i.wvalid) begin 
                     wr_addr_we = 1'b1;
                     wr_data_we = 1'b1;
                     NS = VALID;
@@ -88,7 +82,7 @@ module axi_wr_chan (
                 bvalid_r = 1'b1;
                 bresp_r  = OKAY;
 
-                if (b_ready) NS = READY;
+                if (wr_chan_i.bready) NS = READY;
                 else NS = WAIT_RESP;
             end
 
--- tests/axi_write_channel/tb_wr_chan.sv
+++ tests/axi_write_channel/tb_wr_chan.sv
@@ -2,10 +2,10 @@
 `define TB_WR_CHAN
 
 `include "axi4_itf.sv"
-// `include "axi4_transactions.sv"
+`include "vga_driver_structs.sv"
 
 import axi4_itf::*;
-// import axi4_transactions::*;
+import vga_driver_structs::*;
 
 module tb_wr_chan ();
     localparam AXI_CLK_PERIOD = 15;
@@ -52,11 +52,24 @@ module tb_wr_chan ();
     endtask
 
     // helper functions/task
-    task automatic axi_write_single(
-        input logic [31:0] addr,
-        input logic [7:0]  data
-    );
-        // Send address and data
+    task check_device(
+        input bit [AXI_ADDR_BITS-1:0] expected_wr_addr,
+        input bit [AXI_DATA_BITS-1:0] expected_wr_data);
+
+        wait(wr_valid);
+        @(posedge axi_clk);
+        assert(wr_addr == expected_wr_addr && wr_data == expected_wr_data)
+        else begin
+            $error("check_device failed:\n  Expected: addr = 0x%08h, data = 0x%08h\n  Got:      addr = 0x%08h, data = 0x%08h",
+               expected_wr_addr, expected_wr_data, wr_addr, wr_data);
+            $finish();
+        end;
+    endtask
+
+    task automatic axi_write_single(input logic [31:0] addr,
+        input logic [7:0]  data);
+        
+        @(posedge axi_clk);
         wr_chan_i.awaddr  = addr;
         wr_chan_i.awlen   = 0;
         wr_chan_i.awsize  = 3'b000; // 1 byte
@@ -68,64 +81,32 @@ module tb_wr_chan ();
         wr_chan_i.wlast   = 1;
         wr_chan_i.wvalid  = 1;
 
-        // Wait for both aw and w to be accepted
         wait (wr_chan_o.awready && wr_chan_o.wready);
         @(posedge axi_clk);
 
-        // Deassert after handshake
         wr_chan_i.awvalid = 0;
         wr_chan_i.wvalid  = 0;
 
-        // Wait for bvalid and respond with bready
         wr_chan_i.bready = 1;
+
         wait (wr_chan_o.bvalid);
+        wait(DUT.PS == 2'b00);
 
         @(posedge axi_clk);
-
         wr_chan_i.bready = 0;
-
-        $display("[AXI WRITE] Wrote 0x%02h to 0x%08h", data, addr);
-    endtask
-
-    task check_device(
-        input bit [AXI_ADDR_BITS-1:0] expected_wr_addr,
-        input bit [AXI_DATA_BITS-1:0] expected_wr_data
-        );
-
-        wait(wr_valid);
-        assert(!(wr_addr == expected_wr_addr && wr_data == expected_wr_data))
-        else begin
-            $error("check_device failed:\n  Expected: addr = 0x%08h, data = 0x%08h\n  Got:      addr = 0x%08h, data = 0x%08h",
-               expected_wr_addr, expected_wr_data, wr_addr, wr_data);
-            $finish();
-        end;
     endtask
 
     // test cases
-    
+
     // Tests to see if the interface completes the write channel transaction
     task test_single_write();
         axi_write_single(AXI_CSR_ADDR, 'd1);
     endtask
 
-    // TODO:
-    // task test_burst_write();
-    // endtask
-
-    // test to make sure write transaction sent through the correct response 
-    task test_single_read_from();
-        axi_write_single(AXI_CSR_ADDR, 'd1);
-        check_device(AXI_CSR_ADDR, 'd1);
-    endtask
-
-    // TODO:
-    // task test_multiple_read_from();
-    // endtask
-
     // test to make sure write transaction gets block if fifo was full
     // transaction completes when fifo has room 
     task test_fifo_full_write();
-        #(2 * AXI_CLK_PERIOD)
+        @(posedge axi_clk);
         wr_ready_resp = 1;
 
         // Send address and data
@@ -140,11 +121,11 @@ module tb_wr_chan ();
         wr_chan_i.wlast   = 1;
         wr_chan_i.wvalid  = 1;
 
-        // Wait for both aw and w to be accepted
         wait (!wr_chan_o.awready && !wr_chan_o.wready);
+        @(posedge axi_clk);
+        
         wr_chan_i.awvalid = 0;
         wr_chan_i.wvalid  = 0;
-
         wr_chan_i.bready = 1;
 
         // make i dont get back ready
@@ -159,7 +140,9 @@ module tb_wr_chan ();
         end
 
         wr_ready_resp = 0;
-        wait(wr_chan_o.bvalid);
+        
+        wait (wr_chan_o.bvalid);
+        wait(DUT.PS == 2'b00);
 
         @(posedge axi_clk);
         wr_chan_i.bready = 0;
